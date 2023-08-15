205f5aa36f79cbc998722d5a50cba19987f8629d
unknown <douglas.luzinda.ot@gmail.com>
2023-08-15 19:23:10 +0300

    in love

--- index.js
+++ index.js
@@ -13,24 +13,86 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-if (cluster.isMaster) {
-  for (let i = 0; i < numCpus; i++) {
-    cluster.fork();
-  }
-  
-  cluster.on("exit", (worker, code, signal) => {
-    cluster.fork();
-  });
-} else {
   const server = http.createServer(app);
   const io = require("socket.io")(server, {
     cors: {
       origin: "*"
     }
   });
+  let users = [];
 
-  socketConnections(io)
-
+  const addUser = (userId, socketId) => {
+    if (!users.some(user => user.userId === userId)) {
+      users.push({ userId, socketId });
+    }
+  };
   
+  const removeUser = (socketId) => {
+    users = users.filter(user => user?.socketId !== socketId);
+  };
+  
+  const getUser = (userId) => {
+    return users.find(user => user.userId === userId);
+  };
+
+  io.on("connection", (socket) => {
+    console.log("new socket connection")
+    socket.on("addUser", (userId) => {
+    try{
+      addUser(userId, socket?.id);
+      console.log("New user joined");
+      console.log(users);
+      io.emit("getUsers", users);
+    }catch(err){
+      console.log(err.message)
+    }});
+    socket.on("sendMessage",data=>{
+    try{
+      const user = getUser(data?.id)
+      console.log(data.response[0])
+      console.log(user)
+      io.to(user?.socketId).emit("getMessage",data.response) 
+    }catch(err){
+    console.log(err)
+    }
+    })
+    socket.on("smssent",()=>{
+      console.log("recieved")
+    })
+    socket.on("typing",(data)=>{
+    try{
+      const user = getUser(data.reciever)
+      // { id: 23, reciever: 22 }
+
+      if(user){
+        io.to(user?.socketId).emit("typingIndicator",{"from":data?.id})
+      }
+    }catch(err){
+      console.log(err)
+    }
+    })
+  
+    socket.on("stoptyping",(data)=>{
+    try{
+      const user = getUser(data.reciever)
+      if(user !== null){
+        io.to(user?.socketId).emit("stoptypingIndicator",{"from":data.id})
+      }
+    }catch(err){
+      
+    }
+    })
+  
+  
+    socket.on('disconnect', () => {
+    try{
+      removeUser(socket.id);
+      console.log("User disconnected");
+      console.log(users);
+      io.emit("getUsers", users);
+    }catch(err){
+      
+    }
+    });
+  })
   server.listen(PORT, () => console.log(`🚀 @ http://localhost:${PORT} on pid ${process.pid}`));
-}
